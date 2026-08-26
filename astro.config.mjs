270ad2de467c3b079d718c0f114cb64d074a5c58
eledwel1 <eledwel1@jh.edu>
2026-08-26 10:59:10 -0400

    add deploy workflow and custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://noprincesshockey.com'',
+	site: 'https://www.noprincesshockey.com'',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
